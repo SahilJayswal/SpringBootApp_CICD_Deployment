ead84b3da1af07bb7f00e98501442fae91ca4819
sahil.jayswal <sahil.jayswal1995@gmail.com>
2021-09-18 22:35:04 +0530

    changesmade

--- src/main/java/com/springcicd/test/controller/HomeController.java
+++ src/main/java/com/springcicd/test/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.springcicd.test.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-	@RequestMapping("/home")
+	@GetMapping("/home")
 	public String getname() {
-		return "Sahil hello";
+		return "Sahil";
+	}
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello";
 	}
 }
